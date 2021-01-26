7268d262bbfd0f1d40564008d85ca0d11ff23aac
bwhitmore7 <bryson.whitmore@patelhs.org>
2021-01-26 13:25:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("what is your name?");
+    String name = scan.next();
+    System.out.println("what year is it");
+    int currentYear = scan.nextInt();
+    System.out.println("what is your birth year");
+    int birthYear = scan.nextInt();
+
+    System.out.println(greeting(name));
+  
+    int age = getAge(birthYear, currentYear);
+    System.out.println("your age is" + age);
+  }
+
+//getAge method calculates getAge
+static int getAge(int birthYear, int currentYear)
+ {
+int age = currentYear - birthYear;
+return age;
+ }
+
+ //greeting method returns a personalized greeting
+ static String greeting(String name)
+ {
+  String hello = "hello. welcome back," + name;
+  return hello;
+ }
+
+ //create a method that takes in at least 1 parameter and returns some type 
+ //call this method within your main method just like we did with getAge & greeting
+}
